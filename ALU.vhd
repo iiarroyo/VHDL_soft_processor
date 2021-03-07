fb4591e2cd15078641147b89a217a234511e4cd2
israel-iap <israel.iap@outlook.com>
2021-03-06 20:33:46 -0600

    changed unsigned to std_logic_vector

--- ALU.vhd
+++ ALU.vhd
@@ -4,9 +4,9 @@ Use ieee.numeric_std.all;
 
 Entity ALU is
 Port ( 
-		Sel     : in  unsigned(2 downto 0);
-		InA,InB : in  unsigned(7 downto 0);
-		Oper    : out unsigned(7 downto 0);
+		Sel     : in  std_logic_vector(2 downto 0);
+		InA,InB : in  std_logic_vector(7 downto 0);
+		Oper    : out std_logic_vector(7 downto 0);
 		Zero    : out std_logic
 		);
 End ALU;
@@ -23,32 +23,32 @@ process(Sel,InA,InB)
 begin
 		case Sel is
 			when "000" => -- add
-				Oper_signal <= InA + InB;
+				Oper_signal <= unsigned(InA) + unsigned(InB);
 				Zero <= '0';
 			when "001" => -- sub
-				Oper_signal <= InA - InB;
+				Oper_signal <= unsigned(InA) - unsigned(InB);
 				if(Oper_signal = "00000000") then
 					Zero <= '1';
 				else
 					Zero <= '0';
 				end if;
 			when "010" => -- and
-				Oper_signal <= InA and InB;
+				Oper_signal <= unsigned(InA) and unsigned(InB);
 				Zero <= '0';	
 			when "011" => -- or
-				Oper_signal <= InA or InB;
+				Oper_signal <= unsigned(InA) or unsigned(InB);
 				Zero <= '0';
 			when "100" => -- xor
-			   Oper_signal <= InA xor InB;
+			   Oper_signal <= unsigned(InA) xor unsigned(InB);
 				Zero <= '0';
 			when "101" => -- not
-				Oper_signal <= not(InA);
+				Oper_signal <= not(unsigned(InA));
 				Zero <= '0';
 			when "110" => -- shl
-				Oper_signal <= InA(6 downto 0) & '0';
+				Oper_signal <= unsigned(InA(6 downto 0)) & '0';
 				Zero <= '0';
 			when "111" => -- shr
-				Oper_signal <= '0' & InA(7 downto 1);
+				Oper_signal <= '0' & unsigned(InA (7 downto 1));
 				Zero <= '0';
 			when others => 
 				Oper_signal <= (others => '0');
@@ -57,7 +57,7 @@ begin
 		end case;
 end process;
 			
-Oper <= Oper_signal;
+Oper <= std_logic_vector(Oper_signal);
 
 	
 End a;
